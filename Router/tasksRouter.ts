1b457f0a80926abfff522abb2c683b2b5d494291
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-08 16:58:21 +0200

    finished backend

--- Router/tasksRouter.ts
+++ Router/tasksRouter.ts
@@ -1,6 +1,6 @@
 import { taskController } from './../controller/taskController';
 import { Router, Request, Response } from "express";
-import { taskValidator } from '../src/Validator Schema/TaskValidator';
+import { taskValidator, updateValidator } from '../src/Validator Schema/TaskValidator';
 import { validationResult } from 'express-validator';
 
 export const tasksRouter: Router = Router()
@@ -9,3 +9,10 @@ tasksRouter.get("/get", taskController.gettAll)
 
 
 tasksRouter.post("/post", taskValidator, taskController.create)
+
+tasksRouter.put(
+    '/put',
+    updateValidator,
+    taskController.update,
+);
+
--- controller/taskController.ts
+++ controller/taskController.ts
@@ -1,10 +1,10 @@
 
 import { Task } from "../src/Entitys/tasks.entity";
 import { AppDataSource } from "..";
-import { instanceToPlain } from "class-transformer";
+import { instanceToPlain, plainToInstance } from "class-transformer";
 import { Response, Request } from "express";
 import { validationResult } from "express-validator";
-
+import { UpdateResult } from 'typeorm';
 class TaskController {
 
     public async gettAll(req: Request, res: Response): Promise<Response> {
@@ -52,6 +52,67 @@ class TaskController {
         } catch (err) {
             console.log(err)
             return res.status(500).json({ error: "Internal Server Error" })
+        }
+    }
+
+    public async update(
+        req: Request,
+        res: Response,
+    ): Promise<Response> {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ errors: errors.array() });
+        }
+
+        // Try to find if the tasks exists
+        let task: Task | null;
+
+        try {
+            task = await AppDataSource.getRepository(
+                Task,
+            ).findOne({
+                where: { id: req.body.id },
+            });
+        } catch (errors) {
+            return res
+                .json({ error: 'Internal Server Error' })
+                .status(500);
+        }
+
+        // Return 400 if task is null
+        if (!task) {
+            return res.status(404).json({
+                error: 'The task with given ID does not exist',
+            });
+        }
+
+        // Declare a variable for updatedTask
+        let updatedTask: UpdateResult;
+
+        // Update the task
+        try {
+            updatedTask = await AppDataSource.getRepository(
+                Task,
+            ).update(
+                req.body.id,
+                plainToInstance(Task, {
+                    status: req.body.status,
+                }),
+            );
+
+            // Convert the updatedTask instance to an object
+            updatedTask = instanceToPlain(
+                updatedTask,
+            ) as UpdateResult;
+
+            return res.json(updatedTask).status(200);
+        } catch (errors) {
+            return res
+                .json({ error: 'Internal Server Error' })
+                .status(500);
         }
     }
 }
