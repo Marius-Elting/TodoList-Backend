beb41f38d216d747c2f28144f26a1f94e3c000ff
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-14 20:48:07 +0200

    updated .env copy

--- Router/tasksRouter.ts
+++ Router/tasksRouter.ts
@@ -1,12 +1,11 @@
 import { taskController } from './../controller/taskController';
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { taskValidator, updateValidator } from '../src/Validator Schema/TaskValidator';
-import { validationResult } from 'express-validator';
+
 
 export const tasksRouter: Router = Router()
 
 tasksRouter.get("/get", taskController.gettAll)
-
 
 tasksRouter.post("/post", taskValidator, taskController.create)
 
